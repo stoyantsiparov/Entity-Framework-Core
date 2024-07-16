c81728e8dab3e6a77633d36296892351616399ce
Stoyan Tsiparov <133793181+stoyantsiparov@users.noreply.github.com>
2024-07-16 23:36:24 +0300

    Add Entity Framework Core code

---
+++ 25. Exam Preparation/Cadastre/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Cadastre.Data
+{
+    public class Configuration
+    {
+        public static string ConnectionString =
+            "Server=OMEN\\SQLEXPRESS;Database=Cadastre;Integrated Security=True;";
+    }
+}
